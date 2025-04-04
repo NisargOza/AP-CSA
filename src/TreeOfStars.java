8a5d9b92f59f2f843b04ab47e9cbed2b5833784b
NisargOza <oza.nisarg2009@gmail.com>
2025-04-04 18:34:32 -0400

    Tree of stars exercise

---
+++ src/TreeOfStars.java
@@ -0,0 +1,23 @@
+public class TreeOfStars
+{
+    public static void main(String[] args)
+    {
+        // Call makeATree
+        makeATree();
+    }
+    public static void makeATree()
+    {
+        int distanceFromConsole = 0;
+
+        for(int i = 1; i<=9; i++) {
+            for(int x = 1; x<distanceFromConsole; x++) {
+                System.out.print(" ");
+            }
+            for(int inner = 9; inner >= i; inner--) {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+            distanceFromConsole++;
+        }
+    }
+}
